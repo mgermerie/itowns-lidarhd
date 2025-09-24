iTowns/itowns

fix(Potree1-2): linked to reprojection

packages/Main/src/Parser/Potree2BinParser.js
@@ -37,8 +37,10 @@ export default {
      * @param {ArrayBuffer} buffer - the bin buffer.
      * @param {Object} options
      * @param {string[]} options.in.pointAttributes - the point attributes information contained in metadata.js
-     * @return {Promise} - a promise that resolves with a THREE.BufferGeometry.
+     * @param {THREE.Box3} options.in.bbox - the bbox of the node
+     * @param {THREE.Vector3} options.out.center - the origin position of the data
      *
+     * @return {Promise} - a promise that resolves with a THREE.BufferGeometry.
      */
     parse: async function parse(buffer, options) {
         const metadata = options.in.source.metadata;
@@ -94,6 +96,9 @@ export default {
                 geometry.setAttribute(property, bufferAttribute);
             }
         });
+
+        geometry.userData.origin = options.out.center;
+        geometry.userData.rotation = new THREE.Quaternion();
 
         geometry.computeBoundingBox();
 

packages/Main/src/Parser/PotreeBinParser.js
@@ -69,6 +69,7 @@ export default {
      * @param {ArrayBuffer} buffer - the bin buffer.
      * @param {Object} options
      * @param {string[]} options.in.pointAttributes - the point attributes information contained in cloud.js
+     * @param {THREE.Vector3} options.out.center - the origin position of the data
      * @return {Promise} - a promise that resolves with a THREE.BufferGeometry.
      *
      */
@@ -102,6 +103,9 @@ export default {
             attrOffset = elemOffset;
             geometry.setAttribute(attr.attributeName, new THREE.BufferAttribute(array, attr.numElements, attr.normalized));
         }
+
+        geometry.userData.origin = options.out.center;
+        geometry.userData.rotation = new THREE.Quaternion();
 
         geometry.computeBoundingBox();
 

packages/Main/src/Parser/PotreeCinParser.js
@@ -5,10 +5,12 @@ export default {
     /** Parse .cin PotreeConverter format (see {@link https://github.com/peppsac/PotreeConverter/tree/custom_bin}) and convert to a THREE.BufferGeometry
      * @function parse
      * @param {ArrayBuffer} buffer - the cin buffer.
-     * @return {Promise} - a promise that resolves with a THREE.BufferGeometry.
+     * @param {Object} options
+     * @param {THREE.Vector3} options.out.center - the origin position of the data
      *
+     * @return {Promise} - a promise that resolves with a THREE.BufferGeometry.
      */
-    parse: function parse(buffer) {
+    parse: function parse(buffer, options) {
         if (!buffer) {
             throw new Error('No array buffer provided.');
         }
@@ -28,6 +30,9 @@ export default {
         geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
         geometry.setAttribute('color', new THREE.BufferAttribute(colors, 4, true));
         geometry.boundingBox = box;
+
+        geometry.userData.origin = options.out.center;
+        geometry.userData.rotation = new THREE.Quaternion();
 
         return Promise.resolve(geometry);
     },

mgermerie/itowns-lidarhd

packages/Main/src/Parser/Potree2BinParser.js
@@ -37,8 +37,10 @@ export default {
      * @param {ArrayBuffer} buffer - the bin buffer.
      * @param {Object} options
      * @param {string[]} options.in.pointAttributes - the point attributes information contained in metadata.js
-     * @return {Promise} - a promise that resolves with a THREE.BufferGeometry.
+     * @param {THREE.Box3} options.in.bbox - the bbox of the node
+     * @param {THREE.Vector3} options.out.origin - the origin position of the data
      *
+     * @return {Promise} - a promise that resolves with a THREE.BufferGeometry.
      */
     parse: async function parse(buffer, options) {
         const metadata = options.in.source.metadata;
@@ -94,6 +96,9 @@ export default {
                 geometry.setAttribute(property, bufferAttribute);
             }
         });
+
+        geometry.userData.origin = options.out.origin;
+        geometry.userData.rotation = new THREE.Quaternion();
 
         geometry.computeBoundingBox();
 

packages/Main/src/Parser/PotreeBinParser.js
@@ -69,6 +69,7 @@ export default {
      * @param {ArrayBuffer} buffer - the bin buffer.
      * @param {Object} options
      * @param {string[]} options.in.pointAttributes - the point attributes information contained in cloud.js
+     * @param {THREE.Vector3} options.out.origin - the origin position of the data
      * @return {Promise} - a promise that resolves with a THREE.BufferGeometry.
      *
      */
@@ -102,6 +103,9 @@ export default {
             attrOffset = elemOffset;
             geometry.setAttribute(attr.attributeName, new THREE.BufferAttribute(array, attr.numElements, attr.normalized));
         }
+
+        geometry.userData.origin = options.out.origin;
+        geometry.userData.rotation = new THREE.Quaternion();
 
         geometry.computeBoundingBox();
 

packages/Main/src/Parser/PotreeCinParser.js
@@ -5,10 +5,12 @@ export default {
     /** Parse .cin PotreeConverter format (see {@link https://github.com/peppsac/PotreeConverter/tree/custom_bin}) and convert to a THREE.BufferGeometry
      * @function parse
      * @param {ArrayBuffer} buffer - the cin buffer.
-     * @return {Promise} - a promise that resolves with a THREE.BufferGeometry.
+     * @param {Object} options
+     * @param {THREE.Vector3} options.out.origin - the origin position of the data
      *
+     * @return {Promise} - a promise that resolves with a THREE.BufferGeometry.
      */
-    parse: function parse(buffer) {
+    parse: function parse(buffer, options) {
         if (!buffer) {
             throw new Error('No array buffer provided.');
         }
@@ -28,6 +30,9 @@ export default {
         geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
         geometry.setAttribute('color', new THREE.BufferAttribute(colors, 4, true));
         geometry.boundingBox = box;
+
+        geometry.userData.origin = options.out.origin;
+        geometry.userData.rotation = new THREE.Quaternion();
 
         return Promise.resolve(geometry);
     },