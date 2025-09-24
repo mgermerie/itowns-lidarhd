iTowns/itowns

refactor(LASParser): reproj data during parsing

packages/Main/src/Parser/LASParser.js
@@ -1,5 +1,8 @@
 import * as THREE from 'three';
 import { spawn, Thread, Transfer } from 'threads';
+import proj4 from 'proj4';
+import { OrientationUtils } from '@itowns/geographic';
+import { Coordinates } from 'Main';
 
 let _lazPerf;
 let _thread;
@@ -17,6 +20,22 @@ async function loader() {
     _thread = await spawn(workerInstance());
     if (_lazPerf) { _thread.lazPerf(_lazPerf); }
     return _thread;
+}
+
+function getOrigin(options) {
+    const center = options.out.center;
+    const centerCrsIn = proj4(options.out.crs, options.in.crs).forward(center);
+    return proj4(options.in.crs, options.out.crs).forward([centerCrsIn.x, centerCrsIn.y, 0]);
+}
+
+function getLocalRotation(options, origin) {
+    const isGeocentric = proj4.defs(options.out.crs).projName === 'geocent';
+    let rotation = new THREE.Quaternion();
+    if (isGeocentric) {
+        const coordOrigin = new Coordinates(options.out.crs, origin);
+        rotation = OrientationUtils.quaternionFromCRSToCRS(options.out.crs, 'EPSG:4326')(coordOrigin);
+    }
+    return rotation;
 }
 
 function buildBufferGeometry(attributes) {
@@ -46,8 +65,6 @@ function buildBufferGeometry(attributes) {
     }
     const scanAngle = new THREE.BufferAttribute(attributes.scanAngle, 1);
     geometry.setAttribute('scanAngle', scanAngle);
-
-    geometry.userData.origin = new THREE.Vector3().fromArray(attributes.origin);
 
     return geometry;
 }
@@ -106,15 +123,29 @@ export default {
      */
     async parseChunk(data, options = {}) {
         const lasLoader = await loader();
+        const origin = getOrigin(options);
         const parsedData = await lasLoader.parseChunk(Transfer(data), {
             pointCount: options.in.pointCount,
             header: options.in.header,
-            eb: options.eb,
+            eb: options.in.eb,
             colorDepth: options.in.colorDepth,
+            in: {
+                crs: options.in.crs,
+                projDefs: proj4.defs(options.in.crs),
+            },
+            out: {
+                crs: options.out.crs,
+                projDefs: proj4.defs(options.out.crs),
+                origin,
+            },
         });
 
+        const rotation = getLocalRotation(options, origin);
         const geometry = buildBufferGeometry(parsedData.attributes);
+        geometry.applyQuaternion(rotation);
         geometry.computeBoundingBox();
+        geometry.userData.origin = new THREE.Vector3().fromArray(origin);
+        geometry.userData.rotation = rotation;
         return geometry;
     },
 
@@ -125,9 +156,12 @@ export default {
      * @param {ArrayBuffer} data - The file content to parse.
      * @param {Object} [options]
      * @param {Object} [options.in] - Options to give to the parser.
+     * @param {String} options.in.crs - Crs of the source.
+     * @param {String} options.out.crs - Crs of the view.
      * @param { 8 | 16 } [options.in.colorDepth] - Color depth (in bits).
      * Defaults to 8 bits for LAS 1.2 and 16 bits for later versions
      * (as mandatory by the specification)
+
      *
      * @return {Promise} A promise resolving with a `THREE.BufferGeometry`. The
      * header of the file is contained in `userData`.
@@ -137,16 +171,29 @@ export default {
             console.warn("Warning: options 'skip' not supported anymore");
         }
 
-        const input = options.in;
-
         const lasLoader = await loader();
+        const origin = getOrigin(options);
         const parsedData = await lasLoader.parseFile(Transfer(data), {
-            colorDepth: input?.colorDepth,
+            colorDepth: options.in.colorDepth,
+            in: {
+                crs: options.in.crs,
+                projDefs: proj4.defs(options.in.crs),
+            },
+            out: {
+                crs: options.out.crs,
+                projDefs: proj4.defs(options.out.crs),
+                origin,
+            },
         });
 
+        const rotation = getLocalRotation(options, origin);
         const geometry = buildBufferGeometry(parsedData.attributes);
-        geometry.userData.header = parsedData.header;
+        geometry.applyQuaternion(rotation);
         geometry.computeBoundingBox();
+        geometry.userData.origin = new THREE.Vector3().fromArray(origin);
+        geometry.userData.rotation = rotation;
+        geometry.userData.header = parsedData.header;
+
         return geometry;
     },
 };

mgermerie/itowns-lidarhd

packages/Main/src/Parser/LASParser.js
@@ -1,5 +1,7 @@
 import * as THREE from 'three';
 import { spawn, Thread, Transfer } from 'threads';
+import proj4 from 'proj4';
+import { OrientationUtils, Coordinates } from '@itowns/geographic';
 
 let _lazPerf;
 let _thread;
@@ -17,6 +19,22 @@ async function loader() {
     _thread = await spawn(workerInstance());
     if (_lazPerf) { _thread.lazPerf(_lazPerf); }
     return _thread;
+}
+
+function getOrigin(options) {
+    const center = options.out.origin;
+    const centerCrsIn = proj4(options.out.crs, options.in.crs).forward(center);
+    return proj4(options.in.crs, options.out.crs).forward([centerCrsIn.x, centerCrsIn.y, 0]);
+}
+
+function getLocalRotation(options, origin) {
+    const isGeocentric = proj4.defs(options.out.crs).projName === 'geocent';
+    let rotation = new THREE.Quaternion();
+    if (isGeocentric) {
+        const coordOrigin = new Coordinates(options.out.crs, origin);
+        rotation = OrientationUtils.quaternionFromCRSToCRS(options.out.crs, 'EPSG:4326')(coordOrigin);
+    }
+    return rotation;
 }
 
 function buildBufferGeometry(attributes) {
@@ -46,8 +64,6 @@ function buildBufferGeometry(attributes) {
     }
     const scanAngle = new THREE.BufferAttribute(attributes.scanAngle, 1);
     geometry.setAttribute('scanAngle', scanAngle);
-
-    geometry.userData.origin = new THREE.Vector3().fromArray(attributes.origin);
 
     return geometry;
 }
@@ -106,15 +122,29 @@ export default {
      */
     async parseChunk(data, options = {}) {
         const lasLoader = await loader();
+        const origin = getOrigin(options);
         const parsedData = await lasLoader.parseChunk(Transfer(data), {
             pointCount: options.in.pointCount,
             header: options.in.header,
-            eb: options.eb,
+            eb: options.in.eb,
             colorDepth: options.in.colorDepth,
+            in: {
+                crs: options.in.crs,
+                projDefs: proj4.defs(options.in.crs),
+            },
+            out: {
+                crs: options.out.crs,
+                projDefs: proj4.defs(options.out.crs),
+                origin,
+            },
         });
 
+        const rotation = getLocalRotation(options, origin);
         const geometry = buildBufferGeometry(parsedData.attributes);
+        geometry.applyQuaternion(rotation);
         geometry.computeBoundingBox();
+        geometry.userData.origin = new THREE.Vector3().fromArray(origin);
+        geometry.userData.rotation = rotation;
         return geometry;
     },
 
@@ -125,9 +155,12 @@ export default {
      * @param {ArrayBuffer} data - The file content to parse.
      * @param {Object} [options]
      * @param {Object} [options.in] - Options to give to the parser.
+     * @param {String} options.in.crs - Crs of the source.
+     * @param {String} options.out.crs - Crs of the view.
      * @param { 8 | 16 } [options.in.colorDepth] - Color depth (in bits).
      * Defaults to 8 bits for LAS 1.2 and 16 bits for later versions
      * (as mandatory by the specification)
+
      *
      * @return {Promise} A promise resolving with a `THREE.BufferGeometry`. The
      * header of the file is contained in `userData`.
@@ -137,16 +170,29 @@ export default {
             console.warn("Warning: options 'skip' not supported anymore");
         }
 
-        const input = options.in;
-
         const lasLoader = await loader();
+        const origin = getOrigin(options);
         const parsedData = await lasLoader.parseFile(Transfer(data), {
-            colorDepth: input?.colorDepth,
+            colorDepth: options.in.colorDepth,
+            in: {
+                crs: options.in.crs,
+                projDefs: proj4.defs(options.in.crs),
+            },
+            out: {
+                crs: options.out.crs,
+                projDefs: proj4.defs(options.out.crs),
+                origin,
+            },
         });
 
+        const rotation = getLocalRotation(options, origin);
         const geometry = buildBufferGeometry(parsedData.attributes);
-        geometry.userData.header = parsedData.header;
+        geometry.applyQuaternion(rotation);
         geometry.computeBoundingBox();
+        geometry.userData.origin = new THREE.Vector3().fromArray(origin);
+        geometry.userData.rotation = rotation;
+        geometry.userData.header = parsedData.header;
+
         return geometry;
     },
 };